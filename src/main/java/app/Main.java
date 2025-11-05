2d95c4ac3b4412991973ea909b3c9146c6eb07a8
Eric Xu <eric@Erics-MacBook-Pro-100.local>
2025-11-05 16:09:20 -0500

    added stuff to main and interface adapters

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,16 @@
 package app;
 
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        AppBuilder appBuilder = new AppBuilder();
+        JFrame application = appBuilder
+                .addLoginView()
+                .build();
+
+        application.pack();
+        application.setLocationRelativeTo(null);
+        application.setVisible(true);
+    }
 }
