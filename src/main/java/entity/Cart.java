e3b0799bc7ad41718498bdf0c70c6b893cda09fc
Zhaoning Zhang <robert.zhaoning@Zhaonings-MacBook-Air.local>
2025-11-10 15:51:02 -0500

    Implement Cart Entity corrected

--- src/main/java/entity/Cart.java
+++ src/main/java/entity/Cart.java
@@ -1,4 +1,32 @@
 package entity;
 
 public class Cart {
+
+    private final User owner;
+    private final List<Product> products;
+
+    public Cart(User owner) {
+        this.owner = owner;
+        this.products = new ArrayList<>();
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public List<Product> getProducts() {
+        return Collections.unmodifiableList(products);
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public int getTotalQuantity() {
+        return products.size();
+    }
 }
