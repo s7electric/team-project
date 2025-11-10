556ae986b94953a20c805c7b97bb328f226aa7c3
Yucheng Duan <ethanduan05@gmail.com>
2025-11-10 16:18:05 -0500

    Documentation for Cart entity

--- src/main/java/entity/Cart.java
+++ src/main/java/entity/Cart.java
@@ -1,10 +1,18 @@
 package entity;
 
+/**
+ * This class is an entity representing the shopping cart of Owner.
+ * Cart has its owner, and the products inside the cart
+ * */
 public class Cart {
 
     private final User owner;
     private final List<Product> products;
 
+    /**
+     * Creates a new cart for the owner.
+     * @param owner the owner
+     */
     public Cart(User owner) {
         this.owner = owner;
         this.products = new ArrayList<>();
