4a1338e5be82e201df06f928c4e85c75ae2a58fc
Eric Xu <eric@Erics-MacBook-Pro-100.local>
2025-11-10 16:12:53 -0500

    added product entity

--- src/main/java/entity/Product.java
+++ src/main/java/entity/Product.java
@@ -1,4 +1,33 @@
 package entity;
 
 public class Product {
+
+    private String name;
+    private double price;
+    private String imageUrl;
+    private User seller;
+
+
+    public Product(String name, double price, String imageUrl, User seller) {
+        if (name.isEmpty()) {throw new IllegalArgumentException("Product name cannot be empty");}
+        this.name = name;
+        if (price < 0) {throw new IllegalArgumentException("Product price cannot be negative");}
+        this.price = price;
+        if (imageUrl.isEmpty()) {throw new IllegalArgumentException("Product image url cannot be empty");}
+        this.imageUrl = imageUrl;
+        this.seller = seller;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public User getUser() {
+        return seller;
+    }
+
 }
