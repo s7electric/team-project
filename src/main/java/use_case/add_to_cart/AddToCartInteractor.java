04c4083ae50598f47a049456cf344d4de74df1b9
ericycx <eric.x3254@gmail.com>
2025-11-15 18:05:13 -0500

    initial files for add_to_cart user case

---
+++ src/main/java/use_case/add_to_cart/AddToCartInteractor.java
@@ -0,0 +1,8 @@
+package use_case.add_to_cart;
+
+public class AddToCartInteractor implements AddToCartInputBoundary {
+    private final AddToCartUserDataAccessInterface userDataAccessObject;
+
+    @Override
+    public void execute(AddToCartInputData addToCartInputData) {}
+}
