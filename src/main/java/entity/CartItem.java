2e71685bcef7c37c36e526f0c649512e42151828
Eric Xu <eric.x3254@gmail.com>
2025-11-19 16:16:31 -0500

    new cart item entity and changed cart to a map

---
+++ src/main/java/entity/CartItem.java
@@ -0,0 +1,27 @@
+package entity;
+
+public class CartItem {
+    private final Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void increase(int amount) {
+        quantity += amount;
+    }
+
+    public void decrease(int amount) {
+        quantity -= amount;
+    }
+}
