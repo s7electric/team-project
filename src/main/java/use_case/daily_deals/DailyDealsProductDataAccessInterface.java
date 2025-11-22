5072f88bea7c3f6224f8d0bfaf8426fbe95a40f6
Eric Xu <eric.x3254@gmail.com>
2025-11-22 17:19:12 -0500

    completed product data access interface

--- src/main/java/use_case/daily_deals/DailyDealsProductDataAccessInterface.java
+++ src/main/java/use_case/daily_deals/DailyDealsProductDataAccessInterface.java
@@ -1,15 +1,13 @@
 package use_case.daily_deals;
 
-import entity.User;
+import entity.Product;
+
+import java.util.List;
 
 public interface DailyDealsProductDataAccessInterface {
 
     /**
-     * Checks if the given user exists.
-     * @param username the user to look for
-     * @return true if a user with the given username exists; false otherwise
+     * Returns a list of daily deals.
      */
-    User getUserData(String username);
-
-    void addToCart(User username, Integer productid, Integer quantity);
+    List<Product> getDailyDealsProducts();
 }
