a06825604c98ea13a8ee3332382806817f5bf5b3
s7electric <samiacobazzi@gmail.com>
2025-11-22 14:36:37 -0500

    add products to state

--- src/main/java/interface_adapter/homepage/HomepageState.java
+++ src/main/java/interface_adapter/homepage/HomepageState.java
@@ -1,7 +1,10 @@
 package interface_adapter.homepage;
 
+import entity.Product;
+
 public class HomepageState {
     private String username;
+    private Product[] products;
 
     public HomepageState(String username) {
         this.username = username;
@@ -13,5 +16,13 @@ public class HomepageState {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public void setProducts(Product[] products) {
+        this.products = products;
+    }
+
+    public Product[] getProducts() {
+        return this.products;
     }
 }
