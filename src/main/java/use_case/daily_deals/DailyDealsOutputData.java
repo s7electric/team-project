912023071dd3d9f4684fa765efbe6910e2671fc7
Eric Xu <eric.x3254@gmail.com>
2025-11-22 17:41:41 -0500

    daily deals output data

--- src/main/java/use_case/daily_deals/DailyDealsOutputData.java
+++ src/main/java/use_case/daily_deals/DailyDealsOutputData.java
@@ -5,21 +5,30 @@ import java.util.List;
 /**
  * Output Data for the Add to Cart Use Case.
  */
-class DailyDealsOutputData {
-    List<DealItem> items;
+public class DailyDealsOutputData {
 
-    static class DealItem {
-        Integer productId;
-        String name;
-        String imageUrl;
-        double displayPrice;
-        int discountPercent;
-    }
-    public DailyDealsOutputData(List<DealItem> items) {
-        this.items = items;
-    }
+    public final List<DealItem> items;
 
-    public List<DealItem> getItems() {
-        return items;
+        public static class DealItem {
+            public final Integer productId;
+            public final String name;
+            public final String imageUrl;
+            public final double displayPrice;
+            public final int discountPercent;
+
+            public DealItem(Integer productId, String name, String imageUrl, double displayPrice, int discountPercent) {
+                this.productId = productId;
+                this.name = name;
+                this.imageUrl = imageUrl;
+                this.displayPrice = displayPrice;
+                this.discountPercent = discountPercent;
+            }
+        }
+        public DailyDealsOutputData(List<DealItem> items) {
+            this.items = items;
+        }
+
+        public List<DealItem> getItems() {
+            return items;
+        }
     }
-}
